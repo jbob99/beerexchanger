2d84d81e0ef0440122dcadecd27e84c5bca2bb8c
Bobby <jbob992003@yahoo.com>
2019-01-23 13:28:01 -0500

    updating

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.hpp"
 #include <QApplication>
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 int main(int argc, char *argv[]) {
     srand( (unsigned) time( NULL) );
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+	cout << "Closing up..." << endl;
 
     return a.exec();
 }
