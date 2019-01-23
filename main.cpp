aefa0bcab0e6ccd8db44e27ae1cbe8eec5912eb1
Bobby <jbob992003@yahoo.com>
2019-01-23 13:23:53 -0500

    test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.hpp"
 #include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
     srand( (unsigned) time( NULL) );
     QApplication a(argc, argv);
+	std::cout << "Running ..." << std::endl;
     MainWindow w;
     w.show();
 
