38473775862dec92d0fea4a83d6efda31596cc8b
Bobby <jbob992003@yahoo.com>
2019-01-23 13:26:47 -0500

    removing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include "mainwindow.hpp"
 #include <QApplication>
-#include <iostream>
 
 int main(int argc, char *argv[]) {
     srand( (unsigned) time( NULL) );
     QApplication a(argc, argv);
-	std::cout << "Running ..." << std::endl;
     MainWindow w;
     w.show();
 
